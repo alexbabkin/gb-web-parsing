960574f27b0cf6d5c8a6b3047dfd3517896bf84d
Alexander Babkin <babkin.a@tecomgroup.ru>
2020-06-04 21:15:54 +0300

    added printing of urls

--- lesson-1/task-1.py
+++ lesson-1/task-1.py
@@ -3,7 +3,7 @@ import json
 
 rsp = requests.get('https://api.github.com/users/alexbabkin/repos')
 for repo in rsp.json():
-    print(repo['html_url'])
+    print(repo['name'], repo['html_url'])
 
 with open('task-1-repositories.json', 'w') as f:
     json.dump(rsp.json(), f, indent=4)
