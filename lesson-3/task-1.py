5a436f0670e2b958cd1feff84b91e11ee563be8b
Alexander Babkin <babkin.a@tecomgroup.ru>
2020-06-12 16:11:34 +0300

    added task 3

--- lesson-3/task-1.py
+++ lesson-3/task-1.py
@@ -10,12 +10,14 @@ vacancy_db.delete_many({})
 query_vacancy = input('vacancy: ')
 query_pages = int(input('search pages: '))
 
+# Task 1
 sj_vs = sj_vacancy.get_vacancies(query_pages, query_vacancy)
 hh_vs = hh_vacancy.get_vacancies(query_pages, query_vacancy)
 
 vacancy_db.insert_many(sj_vs)
 vacancy_db.insert_many(hh_vs)
 
+# Task 2
 search_salary_currency = input('salary currency: ').lower()
 search_salary_value = float(input('salary value: '))
 is_minimal = bool(input('should minimal salary be greater than entered? (0 or 1): ') or False)
@@ -31,3 +33,12 @@ else:
 
 for sr in search_result:
     pprint(sr)
+
+# Task 3
+new_vs = sj_vacancy.get_vacancies(query_pages + 1, query_vacancy)
+new_vs.extend(hh_vacancy.get_vacancies(query_pages + 1, query_vacancy))
+
+for v in new_vs:
+    vacancy_db.update_one({'link': v['link']},
+                          {'$set': v},
+                          upsert=True)
